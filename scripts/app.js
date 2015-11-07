fa9dd3db72bc0541b405d23be80ffe004387563d
Jason Tham <jtham@sbcglobal.net>
2015-11-06 23:06:14 -0600

    linked app.js

---
+++ scripts/app.js
@@ -0,0 +1,10 @@
+FB.ui({
+ method: 'share_open_graph',
+ action_type: 'og.likes',
+ action_properties: JSON.stringify({
+   object:'https://developers.facebook.com/docs/',
+ })
+}, function(response){
+ // Debug response (optional)
+ console.log(response);
+});
